80f0cace929f03f713ba224ae7326664c1b360c6
nikkikhou <nikkikhou@gmail.com>
2022-10-29 14:04:36 -0400

    added ability to add notes

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require("express");
 const path = require("path");
 const fs = require("fs");
 const util = require("util");
-const readFromFile = util.promisify(fs.readFile);
+const { debugPort } = require("process");
+
 // call express to object
 const app = express();
 // set port
@@ -10,19 +11,39 @@ const PORT = 3001;
 
 // first middleware
 app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const readFromFile = util.promisify(fs.readFile);
+
+const writeToFile = (destination, content) =>
+  fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+    err ? console.error(err) : console.info(`\nData written to ${destination}`)
+  );
 
 // root route
 app.get("/", (req, res) => res.send("Navigate to /send or /routes"));
 
 // html pages notes
-app.get("/notes", (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/notes.html"))
+);
 
 // api routes
 app.get("/api/notes", (req, res) => {
-    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
-//   res.send("notes");
+  readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
 });
 
+app.post("/api/notes", (req, res) => {
+  const { text, title } = req.body;
+  readFromFile("./db/db.json").then((data) => {
+    let db = JSON.parse(data);
+    db.push({ id: "fakeId", text, title });
+    console.log(db);
+    writeToFile("./db/db.json", db);
+    res.json(db)
+  });
+});
 
 // server
 app.listen(PORT, () =>
